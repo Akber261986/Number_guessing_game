53a9b056256e7a89d04f94983dbbb1c240e4ad8e
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-03-23 03:10:25 +0500

    Pulishing first virsion of  my guess game.

---
+++ index.ts
@@ -0,0 +1,22 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random()* 10 + 1);
+console.log("Welcome to Number Guess Game. ");
+
+const answer = await inquirer.prompt([
+    {
+        name: "guessNumber",
+        type: "number",
+        message: "Please Guess the number between 1 - 10."
+    }
+])
+if (answer.guessNumber === randomNumber){
+        console.log("Congratulations! You Guess the correct Number.");
+    
+    }
+    else{
+            console.log("Sorry!! You Guess the wrong Number.");
+        
+        }
