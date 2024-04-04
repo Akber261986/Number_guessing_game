bbc1c1a2e101c788fab8f3a2c5da2c11a8e8f766
Ghulam Akber <160354667+Akber261986@users.noreply.github.com>
2024-04-05 00:42:29 +0500

    Added some new feature and chalk

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
-const randomNumber = Math.floor(Math.random() * 10 + 1);
-console.log("Welcome to Number Guess Game. ");
-const answer = await inquirer.prompt([
-    {
-        name: "guessNumber",
-        type: "number",
-        message: "Please Guess the number between 1 - 10."
+import chalk from "chalk";
+let randomNumber = Math.floor(Math.random() * 100 + 1);
+console.log(chalk.bgYellow(chalk.black(`WELCOME TO NUMBER GUESSING GAME`)));
+let condition = true;
+let chance = 0;
+while (condition) {
+    let userInput = await inquirer.prompt([
+        {
+            name: "Guess",
+            type: "number",
+            message: "Guess a Number between (1 - 100)"
+        }
+    ]);
+    chance++;
+    if (chance === 10) {
+        console.log(chalk.red(`Sorry!! you have no More chance..`));
     }
-]);
-if (answer.guessNumber === randomNumber) {
-    console.log("Congratulations! You Guess the correct Number.");
-}
-else {
-    console.log("Sorry!! You Guess the wrong Number.");
+    if (randomNumber === userInput.Guess) {
+        console.log(chalk.green `Congratulations!! You Guess the Correct Number.`);
+    }
+    else if (randomNumber > userInput.Guess) {
+        console.log(chalk.yellow(`The Number is Greater then ${userInput.Guess}`));
+    }
+    else if (randomNumber < userInput.Guess) {
+        console.log(chalk.yellow(`The Number is less then ${userInput.Guess} `));
+    }
+    condition = !(randomNumber === userInput.Guess) && (chance != 10);
+    if (randomNumber != userInput.Guess) {
+        console.log(chalk.cyanBright(`You have remaining ${10 - chance} Chances.`));
+    }
+    else {
+        console.log(chalk.blue(`You got the number in ${chance} tries`));
+    }
 }
